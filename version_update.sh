78c9bab2a3eb6f3454591f11d4091504ee9282a1
Girish <79399756+Girish-Anadv-07@users.noreply.github.com>
2023-10-24 02:12:37 -0400

    Update version_update.sh

--- version_update.sh
+++ version_update.sh
@@ -1,8 +1,8 @@
 python_file1="version.py"
 
 version=$(grep "__version_info__ =" "$python_file1" | sed 's/.*\[\(.*\)\].*/\1/')
-
+echo $version
 sed -i -e "1s/s = .*/s = [$version]/" version_update.py
 updated_version=$(python version_update.py)
-
+echo $updated_version
 sed -i -e "1s/__version_info__ = .*/__version_info__ = $updated_version/" version.py
